8365cf017bebc4e0e532153e7531607afdc6fa42
adwaithgireesh06 <adwaithgireesh06@gmail.com>
2026-06-03 22:52:46 +0530

    Create P-02 format specifier.c

---
+++ P-02 format specifier.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float a;
+    int b;
+    a = 3.14;
+    b=12;
+    printf("Displaying a float: %f \n",a);
+    printf("Displaying an integer: %d",b);
+    return 0;
+}
+//using format specifiers inside the printf() statement.
