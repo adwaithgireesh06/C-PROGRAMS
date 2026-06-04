13ec41e4fb3e7c3fffd87c4b5750363a04dd547c
adwaithgireesh06 <adwaithgireesh06@gmail.com>
2026-06-04 14:40:37 +0530

    Refactor multiplication_table.c for cleaner formatting
    Removed unnecessary blank lines for cleaner code.

--- P-10 multiplication_table.c
+++ P-10 multiplication_table.c
@@ -3,11 +3,9 @@ int main() {
     int tables_num;
     printf("Enter a number to display multiplication tables upto that number: ");
     scanf("%d",&tables_num);
-    
     int limit;
     printf("Enter the limit of all tables: ");
     scanf("%d",&limit);
-    
     for (int i=1; i<=tables_num; i++) {
         printf("Multiplication table of %d:\n",i);
         for (int k=1; k<=limit; k++) {
